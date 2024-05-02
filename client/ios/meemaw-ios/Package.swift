c5597e3cd3d89afdada8d57cbacde35b9c029f1b
Marceau Lecomte <marceaul@users.noreply.github.com>
2024-05-02 16:56:00 +0200

    iOS SDK name change

--- client/ios/meemaw-ios/Package.swift
+++ client/ios/meemaw-ios/Package.swift
@@ -3,26 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "meemaw-ios",
+    name: "meemaw",
     platforms: [
         SupportedPlatform.iOS(.v13),
         SupportedPlatform.macOS(.v11)
     ],
     products: [
         .library(
-            name: "meemaw-ios",
-            targets: ["meemaw-ios"]),
+            name: "meemaw",
+            targets: ["meemaw"]),
     ],
     dependencies: [
         .package(url: "https://github.com/argentlabs/web3.swift", from: "1.1.0")
     ],
     targets: [
         .target(
-            name: "meemaw-ios",
+            name: "meemaw",
             dependencies: [.target(name: "Tsslib"), "web3.swift"]),
         .binaryTarget(
             name: "Tsslib",
-            url: "https://github.com/getmeemaw/meemaw/releases/download/v0.1.0/Tsslib.xcframework.zip";
-            checksum: "8a1acdd534a99753da947d5d806d68799ab9115bf3af8d601be20e2b81402740"
+            url: "https://github.com/getmeemaw/meemaw/releases/download/v0.1.1/Tsslib.xcframework.zip",
+            checksum: "8a1acdd534a99753da947d5d806d68799ab9115bf3af8d601be20e2b81402740")
     ]
 )
